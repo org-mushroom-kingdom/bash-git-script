121c22af174f639654e1975ce9d557f557e6341b
Matt C <mcumm64@gmail.com>
2025-08-31 17:58:24 -0400

    try just call, specifiy fxn as arg

--- shell-scripts/divide.sh
+++ shell-scripts/divide.sh
@@ -7,14 +7,16 @@
 # $ source ./shell-scripts/divide.sh
 # function_name arg1 arg2 arg3 ...
 
+FXN_TO_CALL=$1
+NUM_TO_DIVIDE=$2
 
 is_divisible_by_2()
 {
-    NUM_TO_DIVIDE=$1 # Integer
+    NUM=$1 # Integer
     DIVIDES_BY_2=false
 
-    echo "NUM_TO_DIVIDE = $NUM_TO_DIVIDE"
-    if (($NUM_TO_DIVIDE % 2 == 0))
+    echo "NUM = $NUM"
+    if (($NUM % 2 == 0))
     then
         echo "Divisible by 2"
         DIVIDES_BY_2=true
@@ -22,7 +24,12 @@ is_divisible_by_2()
         echo "NOT Divisible by 2"
         DIVIDES_BY_2=false
     fi
-    echo "NUM_TO_DIVIDE divisible by 2 = ${DIVIDES_BY_2}"
+    echo "${DIVIDES_BY_2}"
 }
+
+if ( "$FXN_TO_CALL" = "is_divisible_by_2")
+then
+    echo "$NUM_TO_DIVIDE is_divisible_by_2 = " $(is_divisible_by_2 $NUM_TO_DIVIDE)
+fi
 
 echo "DIVIDING"
