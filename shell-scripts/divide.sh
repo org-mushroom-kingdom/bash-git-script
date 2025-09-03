f4ba76a4c65fdb7d89887bb2e4cf7fb346ade297
Matt C <mcumm64@gmail.com>
2025-09-02 22:05:13 -0400

    comment cleanup, try w single brackets

--- shell-scripts/divide.sh
+++ shell-scripts/divide.sh
@@ -10,17 +10,11 @@
 FXN_TO_CALL=$1
 NUM_TO_DIVIDE=$2
 
-# The current situation is that $NUM_TO_DIVIDE appears to not be passing to is_divisible_by_2, resulting in
-# ./shell-scripts/divide.sh: line 21: ((: = % 2 == 0: syntax error: operand expected (error token is "= % 2 == 0")
-# ./shell-scripts/divide.sh: line 36: is: command not found
-# BUT echoing NUM_TO_DIVIDE on its own is working so maybe above errors due to something else? Look in Bash docs
 is_divisible_by_2()
 {
     NUM=$1 # Integer
     DIVIDES_BY_2="is false!!"
 
-    # echo "NUM = $NUM"
-    # echo "${NUM}"
     if (( NUM % 2 == 0 ))
     then
         # echo "Divisible by 2"
@@ -32,12 +26,11 @@ is_divisible_by_2()
     echo "${DIVIDES_BY_2}"
 }
 
-if [[ "$FXN_TO_CALL" == "is_divisible_by_2" ]]
+# Double brackets in bash 
+if [ "$FXN_TO_CALL" == "is_divisible_by_2" ]
 then
     # echo "NUM_TO_DIVIDE = $NUM_TO_DIVIDE"
     # echo "$NUM_TO_DIVIDE is_divisible_by_2 = " $(is_divisible_by_2 $NUM_TO_DIVIDE)
     IS_IT_DIVISIBLE=$(is_divisible_by_2 "${NUM_TO_DIVIDE}")
     echo -n $IS_IT_DIVISIBLE | tr -d '\n'
 fi
-# echo "NUM_TO_DIVIDE = $NUM_TO_DIVIDE"
-# echo "DIVIDING"
