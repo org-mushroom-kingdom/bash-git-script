f6ce8b5dc501a1329d739a94053152b25e82a54f
Matt C <mcumm64@gmail.com>
2025-09-05 18:52:23 -0400

    simple call to pr-labels

--- shell-scripts/pr-labels.sh
+++ shell-scripts/pr-labels.sh
@@ -4,8 +4,14 @@
 
 # gh auth login
 
-gh api \
---method GET \
--H "Accept: application/vnd.github+json" \
--H "X-GitHub-Api-Version: 2022-11-28" \
-/orgs/org-mushroom-kingdom/teams/team-peach/members
+# Get the members of a team
+# gh api \
+# --method GET \
+# -H "Accept: application/vnd.github+json" \
+# -H "X-GitHub-Api-Version: 2022-11-28" \
+# /orgs/org-mushroom-kingdom/teams/team-peach/members
+
+# Add labels to PR based on target branch
+# Given a PR number from test-pr-action
+pr_number=$1
+echo "pr-labels.sh says: pr_number = $pr_number" 
