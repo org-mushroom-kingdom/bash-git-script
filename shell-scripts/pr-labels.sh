842c97ceee6ea170e3730133e79e0ce4103de80c
Matt C <mcumm64@gmail.com>
2025-09-06 20:40:04 -0400

    See what happens with CLI API call

--- shell-scripts/pr-labels.sh
+++ shell-scripts/pr-labels.sh
@@ -11,6 +11,9 @@
 # -H "X-GitHub-Api-Version: 2022-11-28" \
 # /orgs/org-mushroom-kingdom/teams/team-peach/members
 
+gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/org-mushroom-kingdom/teams/team-peach/members
+
+
 # Add labels to PR based on target branch
 
 # We can reference $PR_NUMBER from test-pr-action 
