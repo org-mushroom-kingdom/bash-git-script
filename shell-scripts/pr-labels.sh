ca487a70c3de517a77206a27abff6ff31424d4be
Matt C <mcumm64@gmail.com>
2025-09-07 00:38:58 -0400

    Several more comments, scaff >> pr-labels

--- shell-scripts/pr-labels.sh
+++ shell-scripts/pr-labels.sh
@@ -17,20 +17,47 @@
 # Use string/array to add labels to the PR
 # 
 
+declare -a LABEL_LIST=()
+
 # This gives a JSON array of teams, which we want the value that corresponds to the 'name' key
 # Use \ to split the command into multiline
-# Use jq command line tool to process JSON
-TEAMS=$(gh api \
+# Use jq command line tool to process JSON: Bash jq is like sed for JSON. map(.name) takes the value of each JSON's 'name' key and throws it into array
+# This is an example of using Github CLI (with jq) to call the Github API
+
+#TODO: don't hard code to org name, use an ORG var in test-pr-action and pass it in that way. Make sure to Ctrl+F for org-mushroom-kingdom and update all refs
+TEAM_NAMES=$(gh api \
 -H "Accept: application/vnd.github+json" \
 -H "X-GitHub-Api-Version: 2022-11-28" \
 -H "Authorization: Bearer $TEAMS_READ_TOKEN" \
 orgs/org-mushroom-kingdom/teams | jq 'map(.name)')
 
-echo "TEAMS = $TEAMS"
+echo "TEAMS = $TEAM_NAMES"
 
+# For each team, look to see if $PR_CREATOR is a part of that team
+# By getting the members of that team (array), then seeing if $PR_CREATOR is in that array
+# for team in "${TEAM_NAMES[@]}"
+# do
+#   Use jq to get array of usernames in that team
+#   TEAM_MEMBERS=$(gh api \
+#   -H "Accept: application/vnd.github+json" \
+#   -H "X-GitHub-Api-Version: 2022-11-28" \
+#   -H "Authorization: Bearer $TEAMS_READ_TOKEN"  \
+#   /orgs/org-mushroom-kingdom/teams/$team/members | jq 'map(.login)')
 
+# ex. [team-mario would be ["mcummings128"], team-peach would be ["mcummings128","mcummings129"]
+# for username in "${TEAM_MEMBERS[@]}"
+# do
+#   if [[ "${username}" == "{$PR_CREATOR}"" ]]
+#   then
+#       # Assuming team name is same as label name...
+#       LABEL_LIST+=($team)
+#       Let's say it wasn't though. Like team-luigi has a corresponding 'luigi' label
+#       Or there's a 'bowser' team with no 'team-' prefix
+#   done
+# done
 
 # gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/org-mushroom-kingdom/teams/team-peach/members
+echo -e "team-peach members below: \n"
 gh api --method GET -H "Authorization: Bearer $TEAMS_READ_TOKEN"  /orgs/org-mushroom-kingdom/teams/team-peach/members
 
 
