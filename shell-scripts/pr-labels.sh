1505a759ffc8c31fb9371c6ecf505723e9e5d565
Matt C <mcumm64@gmail.com>
2025-09-07 21:16:39 -0400

    team_label scaff, comments >> pr-labels

--- shell-scripts/pr-labels.sh
+++ shell-scripts/pr-labels.sh
@@ -23,7 +23,7 @@
 # Use string/array to add labels to the PR
 # 
 
-declare -a LABEL_LIST=()
+declare -a TEAM_LABEL_LIST=()
 
 # This gives a JSON array of teams, which we want the value that corresponds to the 'name' key
 # Use \ to split the command into multiline
@@ -39,6 +39,7 @@ orgs/org-mushroom-kingdom/teams | jq 'map(.name)')
 
 echo "TEAMS = $TEAM_NAMES"
 
+# TODO: fxn? Name something like add_team_labels
 # For each team, look to see if $PR_CREATOR is a part of that team
 # By getting the members of that team (array), then seeing if $PR_CREATOR is in that array
 for team in "${TEAM_NAMES[@]}"
@@ -56,24 +57,30 @@ do
   # [@] expands the array so each element is a separate word. Each element/word is considered a separate argument for printf
   printf "%s\\n" "${TEAM_MEMBERS[@]}"
 
+# Loop thru the members of a team. If the PR_CREATOR == username in team, add the corresponding team label
 # ex. [team-mario would be ["mcummings128"], team-peach would be ["mcummings128","mcummings129"]
-# for username in "${TEAM_MEMBERS[@]}"
-# do
-#   if [[ "${username}" == "{$PR_CREATOR}" ]]
-#   then
-#       # Assuming team name is same as label name, add to array (+=)
-#       LABEL_LIST+=($team)
-#       Let's say it wasn't though. Like team-luigi has a corresponding 'luigi' label
-#       Or there's a 'bowser' team with no 'team-' prefix
-#       Probably have some sort of JSON or csv file where headers are team, labels
-#       So if username=$PR_CREATOR, then look at $team in JSON/csv and get corresponding value 
-#   done
+for username in "${TEAM_MEMBERS[@]}"
+do
+  if [[ "${username}" == "{$PR_CREATOR}" ]]
+  then
+      # Assuming team name is same as label name, add to array (+=)
+      TEAM_LABEL_LIST+=($team)
+    #   Let's say it wasn't though. Like team-luigi has a corresponding 'luigi' label
+    #   Or there's a 'bowser' team with no 'team-' prefix
+    #   Probably have some sort of JSON or csv file where headers are team, labels
+    #   So if username=$PR_CREATOR, then look at $team in JSON/csv and get corresponding value 
+  done
 done
 
 # gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/org-mushroom-kingdom/teams/team-peach/members
 echo -e "team-peach members below: \n"
 gh api --method GET -H "Authorization: Bearer $TEAMS_READ_TOKEN"  /orgs/org-mushroom-kingdom/teams/team-peach/members
 
+# Loop thru TEAM_LABEL_LIST and add all the labels
+for team_label in "${TEAM_LABEL_LIST[@]}"
+do
+    gh pr edit "$PR_NUMBER" --add-label "$team_label"
+done
 
 # Add labels to PR based on target branch
 
@@ -83,6 +90,9 @@ echo -e "\n pr-labels.sh says: pr_number = ${PR_NUMBER}"
 
 echo "Target branch = ${TARGET_BRANCH} ."
 
+#TODO: fxn? Name like add_branch_labels
+# Assume all environment branches begin with env. Use cut and -d for delimiter, with / being the delimiter. Get the 2nd piece of that cut (stuff after /)
+# Also assume that env labels are just the name of the environment branch after the / (ex. env/dev has corresponding 'dev' label)
 env_label=$(echo "$TARGET_BRANCH" | cut -d'/' -f2)
 echo "env_label = $env_label"
 
