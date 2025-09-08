0db2b823ad31808c640f34379cd16e224476b3ee
Matt C <mcumm64@gmail.com>
2025-09-07 23:20:08 -0400

    Just .[] no slug >> pr-labels

--- shell-scripts/pr-labels.sh
+++ shell-scripts/pr-labels.sh
@@ -41,12 +41,12 @@ declare -a TEAM_LABEL_LIST=()
 # Use jq command line tool to process JSON: Bash jq is like sed for JSON.
 # Pipe with 'jq .[].slug' which breaks down to: 
 # . = filter for current JSON (result of gh api). If we just did jq . it would give the output of the JSON array (though it looks a little different when testing with the Actions page vs gh api with no jq present)
-# [] = iterate thru each element of JSON array 
+# [] = iterate thru each element of JSON array and give a separate output for each
 # .slug = get the 'slug' value
 # Use mapfile to read lines and assign each line an index of an array (TEAM_NAMES). -t removes trailing newline chars
 # Mapfile reads input--we need to feed the output of jq as input. Use process substituion
 # mapfile -t TEAM_NAMES < <(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer $TEAMS_READ_TOKEN" orgs/$ORG/teams | jq '.[].slug')
-gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer $TEAMS_READ_TOKEN" orgs/$ORG/teams | jq '.[].slug'
+gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer $TEAMS_READ_TOKEN" orgs/$ORG/teams | jq '.[]'
 
 # echo "TEAMS = ${TEAM_NAMES[@]}"
 # echo "repo owner = $ORG"
