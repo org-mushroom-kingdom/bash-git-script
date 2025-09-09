ba3ef692778c91df81ae6c4c138ec220a2037377
Matt C <mcumm64@gmail.com>
2025-09-09 16:36:06 -0400

    Test getCodeonwers CLI >> test-pr-axn2, rd-thru-co

--- shell-scripts/read-thru-codeowners.sh
+++ shell-scripts/read-thru-codeowners.sh
@@ -2,9 +2,16 @@
 
 # This is its own separate script for a couple of reasons:
 # 1. As an example of how a script can be called from another script (main-script.sh calls this one)
-# 2. Possible use in Github Action test-pr-action-1.yml
+# 2. As an example of how to call a script from Github Actions
+# 3. Some third reason TBD
+
+
+# This is called from the test-pr-action. It depends the following variables being defined in the action
+# TARGET_BRANCH, PR_NUMBER, ORG
 
 echo "read_thru_codeowners.sh was hit!"
 
+
 #Open/Get CODEOWNERS
 # Get filepath (in PR), see if path is in 
+gh api repos/{$ORG}/contents/.gitignore/CODEOWNERS
