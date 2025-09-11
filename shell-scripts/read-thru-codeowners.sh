6984ea3f4d80fa93d270950586b5b8f7fb584c01
Matt C <mcumm64@gmail.com>
2025-09-10 20:08:51 -0400

    test more codeowners stuff, clarity

--- shell-scripts/read-thru-codeowners.sh
+++ shell-scripts/read-thru-codeowners.sh
@@ -11,8 +11,24 @@
 
 echo "read_thru_codeowners.sh was hit!"
 
+declare -a codeowners_lines
 
 #Open/Get CODEOWNERS via Github CLI/Github API
-# Get filepath (in PR), see if path is in 
-gh api repos/${REPO_PATH}/contents/.gitignore/CODEOWNERS | jq '.content' | base64 --decode 
 
+
+# gh api repos/${REPO_PATH}/contents/.gitignore/CODEOWNERS | jq -r '.content' | base64 --decode 
+# gh api cmd here gives a million outputs, so use mapfile to put them in arr
+mapfile -t codeowners_raw_lines < <(gh api repos/${REPO_PATH}/contents/.gitignore/CODEOWNERS | jq -r '.content' | base64 --decode )
+
+# Get filepath (in PR), see if path is in 
+
+echo "codeowners_raw_lines[0] = ${codeowners_lines[0]}"
+echo "codeowners_raw_lines[1] = ${codeowners_lines[1]}"
+
+# for line in "${codeowners_lines[@]}"
+# do
+#     if [[ "${line}" != "#"* ]]
+#     then
+#         codeowners_lines+=($line)
+#     fi
+# done
