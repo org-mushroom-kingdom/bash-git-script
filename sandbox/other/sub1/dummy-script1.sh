a971582818e09048e14cc91a4c8cc73e31394a24
Matt C <mcumm64@gmail.com>
2025-09-23 01:06:06 -0400

    Try w nested ext

---
+++ sandbox/other/sub1/dummy-script1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This is a dummy file. It just serves to be something in /sandbox/other/sub1/ other than a .txt file for CODEOWNERS testing purposes.
+# Since this is a .sh file in /sandbox/other/sub1/ it should be owned by Mario's team.
