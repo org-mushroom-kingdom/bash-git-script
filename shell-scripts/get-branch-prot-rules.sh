0719addc043bf50f77fdbb5aa4706848afa36fef
Matt C <mcumm64@gmail.com>
2025-09-29 23:35:33 -0400

    Here goes nothing >> g-b-p-r.sh

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "You picked $GET_RULES_FOR "
+
+if [[ "$GET_RULES_FOR" != "all" ]]
+then
+    ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/branches/$GET_RULES_FOR/protection)
+    echo "Branch ruleset: $ruleset"
+else
+    echo "'all' logic TBD'"
