ae685b875f1a4a357349a5b4f73a31a39b3a0948
Matt C <mcumm64@gmail.com>
2025-09-30 23:04:18 -0400

    upd branch-prot stuff with REPO_READ token

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# This gets the branch protection rules based on what the user input was in get-branch-protection-rules.yml (default is all)
+
 echo "You picked $GET_RULES_FOR "
 
 if [[ "$GET_RULES_FOR" != "all" ]]
 then
-    ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/branches/$GET_RULES_FOR/protection)
+    ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/branches/$GET_RULES_FOR/protection --header "Authorization: Bearer $REPO_READ_TOKEN")
     echo "Branch ruleset: $ruleset"
 else
     echo "'all' logic TBD'"
