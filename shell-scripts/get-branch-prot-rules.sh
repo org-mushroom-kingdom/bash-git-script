c02c4d83a13127884f35f0f056835683ea1a3beb
Matt C <mcumm64@gmail.com>
2025-10-01 00:11:04 -0400

    Update get-branch-prot-rules.sh

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -10,12 +10,16 @@ then
     # ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/branches/env/qa1/protection -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
     # ruleset=$(gh api repos/org-mushroom-kingdom/bash-git-script/rules/branches -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
     # ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/rulesets/8111052 -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
-    ruleset=$(gh api \
+    mapfile -t ruleset_ids < <(gh api \
     -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     -H "Authorization: Bearer $REPO_READ_TOKEN" \
-    repos/org-mushroom-kingdom/bash-git-script/rulesets)
-    echo "Branch ruleset: $ruleset"
+    repos/org-mushroom-kingdom/bash-git-script/rulesets | jq -r '.[].id')
+    # echo "Branch ruleset: $ruleset"
+    for id in "$ruleset_ids[@]"
+    do
+        echo "Branch ruleset id: $id"
+    done 
 else
-    echo "'all' logic TBD'"
+    echo "Specific logic TBD'"
 fi
