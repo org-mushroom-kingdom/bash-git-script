83127829464a4fb475f4ada6287118dfe00c38ae
Matt C <mcumm64@gmail.com>
2025-10-01 00:04:49 -0400

    Update get-branch-prot-rules.sh

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -9,8 +9,12 @@ then
     BRANCH="env%2Fqa1"
     # ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/branches/env/qa1/protection -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
     # ruleset=$(gh api repos/org-mushroom-kingdom/bash-git-script/rules/branches -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
-    # ruleset=$(gh api repos/org-mushroom-kingdom/bash-git-script/rulesets -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
-    ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/rulesets/8111052 -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
+    # ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/rulesets/8111052 -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
+    ruleset=$(gh api \
+    repos/org-mushroom-kingdom/bash-git-script/rulesets 
+    \ -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -H "Authorization: Bearer $REPO_READ_TOKEN")
     echo "Branch ruleset: $ruleset"
 else
     echo "'all' logic TBD'"
