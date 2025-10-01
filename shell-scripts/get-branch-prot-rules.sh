6d10c884b3a502dd974541d46fff5affc5401cf6
Matt C <mcumm64@gmail.com>
2025-09-30 23:28:41 -0400

    Update get-branch-prot-rules.sh

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -7,7 +7,7 @@ echo "You picked $GET_RULES_FOR "
 if [[ "$GET_RULES_FOR" == 'all branches with rules' ]]
 then
     BRANCH="env%2Fqa1"
-    ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/branches/env%2Fqa1/protection -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
+    ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/branches/env%2Fqa1/protection -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: token $REPO_READ_TOKEN")
     echo "Branch ruleset: $ruleset"
 else
     echo "'all' logic TBD'"
