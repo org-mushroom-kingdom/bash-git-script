039b404050842df444124ef5e6c01d4e37ff5878
Matt C <mcumm64@gmail.com>
2025-10-02 01:15:04 -0400

    diff jq syntax >> g-b-p-r.sh

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -19,10 +19,11 @@ then
     for id in "${ruleset_ids[@]}"
     do
         # echo "Branch ruleset id: $id"
-        ruleset_name=$(gh api /repos/org-mushroom-kingdom/bash-git-script/rulesets/$id -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN" | jq '.name') 
-        all_rules_json_arr+=($ruleset_name)
+        ruleset_json=$(gh api /repos/org-mushroom-kingdom/bash-git-script/rulesets/$id -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN") 
+        # ruleset_name=$(gh api /repos/org-mushroom-kingdom/bash-git-script/rulesets/$id -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN" | jq '.name') 
+        all_rules_json_arr+=($ruleset_json | jq -r '.name')
         #TODO: Use this as a scaffold to write to a file
-        echo "$changed_files_output" > test-json-output.txt 
+        # echo "$changed_files_output" > test-json-output.txt 
         # git add test-json-output.txt
         # git commit -m "Capture output from Github 'get changed files' call"
         # git push origin main
