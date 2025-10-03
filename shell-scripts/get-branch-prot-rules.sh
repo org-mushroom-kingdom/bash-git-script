1ca1938070c8f47d4d65f489f879d1069b946b21
Matt C <mcumm64@gmail.com>
2025-10-02 20:31:12 -0400

    init rule_chunk >> g-b-p-r.sh

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -5,16 +5,24 @@
 # TODO: It would be great to schedule this like once a week/day to get the most current info on the rules
 echo "You picked $GET_RULES_FOR "
 declare -a all_rules_json_arr
-    BRANCH="env%2Fqa1"
-    # ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/branches/env/qa1/protection -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
-    # ruleset=$(gh api repos/org-mushroom-kingdom/bash-git-script/rules/branches -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
-    # ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/rulesets/8111052 -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
-    mapfile -t ruleset_ids < <(gh api \
-    -H "Accept: application/vnd.github+json" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    -H "Authorization: Bearer $REPO_READ_TOKEN" \
-    repos/org-mushroom-kingdom/bash-git-script/rulesets | jq -r '.[].id')
-    # echo "Branch ruleset: $ruleset"
+BRANCH="env%2Fqa1"
+# ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/branches/env/qa1/protection -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
+# ruleset=$(gh api repos/org-mushroom-kingdom/bash-git-script/rules/branches -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
+# ruleset=$(gh api /repos/org-mushroom-kingdom/bash-git-script/rulesets/8111052 -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN")
+mapfile -t ruleset_ids < <(gh api \
+-H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+-H "Authorization: Bearer $REPO_READ_TOKEN" \
+repos/org-mushroom-kingdom/bash-git-script/rulesets | jq -r '.[].id')
+# echo "Branch ruleset: $ruleset"
+
+rule_chunk()
+{
+    rule_json_str="$1"
+    rule_name=$(echo "" | jq '.name')
+    echo "rule_name = $rule_name"
+}
+
 if [[ "$GET_RULES_FOR" == 'all branches with rules' ]]
 then
     for id in "${ruleset_ids[@]}"
@@ -24,19 +32,25 @@ then
         # ruleset_name=$(gh api /repos/org-mushroom-kingdom/bash-git-script/rulesets/$id -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $REPO_READ_TOKEN" | jq '.name') 
         echo "$ruleset_json"
         # Example of how to filter a single JSON for the desired values (ex. original JSON returns things like id, source_type, etc) 
-        # This essentially creates a new JSON
+        # This essentially creates a new JSON (note the {})
         # The 'effected_branches:' syntax creates a key called 'effected_branches' in the new JSON. 
         # In ruleset_json, 'conditions' has a JSON value. The 'ref_name' key in THAT JSON is another JSON. 'include' is a key (with an array value) in the ref_name JSON.
         ruleset_json=$(echo "$ruleset_json" | jq '{name, effected_branches: .conditions.ref_name.include, enforcement, rules, updated_at}') 
         
         all_rules_json_arr+=("$ruleset_json")
-        #TODO: Use this as a scaffold to write to a file
-        # echo "$changed_files_output" > test-json-output.txt 
-        # git add test-json-output.txt
-        # git commit -m "Capture output from Github 'get changed files' call"
-        # git push origin main
     done
-    echo "all_rules_json_arr[0] = ${all_rules_json_arr[0]}" 
+    echo "all_rules_json_arr[0] = ${all_rules_json_arr[0]}"
+    for rule_json in $all_rules_json_arr
+    do
+        rule_chunk $rule_json
+    done
+    
+    # TODO: Use this as a scaffold to push to a file
+    # echo "$changed_files_output" > ./docs/branch-protection-rules.md 
+    # git add ./docs/branch-protection-rules.md #prepare for commit
+    # git commit -m "Update branch-protection-rules"
+    # git push origin main
+
 else
     echo "Specific logic TBD'"
 fi
