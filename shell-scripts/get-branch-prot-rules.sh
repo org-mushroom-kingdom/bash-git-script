2da79c423832d46f3c80ff838302fedfe2d16d61
Matt C <mcumm64@gmail.com>
2025-10-05 00:12:07 -0400

    Update get-branch-prot-rules.sh

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -51,7 +51,8 @@ add_rule_chunk()
         rule_chunk+="    - $effected $br"
     done
     mapfile -t rule_json_arr < <(echo "$rule_json_str" | jq -r '.rules')
-    echo "rule_json_arr[0] = ${rule_json_arr[0]}"
+    # echo "rule_json_arr[0] = ${rule_json_arr[0]}"
+    echo "rule_json_arr = ${rule_json_arr}"
     mapfile -t rule_ruletype_list < <(echo "$rule_json_str" | jq -r '.rules' | jq -r '.[].type')
     # echo "rule_rules = $rule_rules"
     # echo "rule_ruletype_list[0] = ${rule_ruletype_list[0]}"
