0d10ee14bfafb2293a3318eebae503d8b7fb0011
Matt C <mcumm64@gmail.com>
2025-10-05 00:58:50 -0400

    Update get-branch-prot-rules.sh

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -54,8 +54,8 @@ add_rule_chunk()
     # rule_json=$(echo "$rule_json_str" | jq -r '.[].rules') This doesn't work Cannot index string with string 'rules'
     rule_json=$(echo "$rule_json_str" | jq -c '.rules[]')
     # echo "rule_json = ${rule_json}"
-    rule_json_type=$(echo "$rule_json" | jq -r '.type')
-    echo "rule_json_type = $rule_json_type"
+    # rule_json_type=$(echo "$rule_json" | jq -r '.type')
+    # echo "rule_json_type = $rule_json_type"
     # for rule_json in "${rule_json_arr[@]}"
     # do
     #     echo "rule_json = ${rule_json}"
