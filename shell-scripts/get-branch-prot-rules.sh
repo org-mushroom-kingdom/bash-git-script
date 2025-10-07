d4284d63ecc10e98b2b3066ec9c4ad93c7e69e4b
Matt C <mcumm64@gmail.com>
2025-10-06 23:10:01 -0400

    diff not null syntax

--- shell-scripts/get-branch-prot-rules.sh
+++ shell-scripts/get-branch-prot-rules.sh
@@ -67,7 +67,7 @@ add_rule_chunk()
         rule_json_parameters=$(echo "$rule_json" | jq -r '.parameters')
         # If not null then...
         # Some method
-        if [[ ! -z "$rule_json_parameters" ]]
+        if [[ $rule_json_parameters != null ]]
         then
             echo "JSON with type ${rule_json_type} has a parameters key"
             echo "rule_json_parameters = $rule_json_parameters"
